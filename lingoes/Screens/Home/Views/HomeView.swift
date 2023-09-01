404da4a33b47ac82742658cf943da989be5e625e
Guen <heloo@guen.dev>
2023-09-01 21:41:02 +0700

    cm

---
+++ lingoes/Screens/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  lingoes
+//
+//  Created by Guen on 01/09/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ lingoes/Screens/Onboarding/Views/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  lingoes
+//
+//  Created by Guen on 01/09/2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Onbroading")
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
