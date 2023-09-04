875403a70472132c97a07e52d9848d07dae46cde
Guen <heloo@guen.dev>
2023-09-04 18:39:02 +0700

    cm

---
+++ lingoes/Screens/Overlook/Views/OverlookView.swift
@@ -0,0 +1,93 @@
+//
+//  OverlookView.swift
+//  lingoes
+//
+//  Created by Guen on 03/09/2023.
+//
+
+import SwiftUI
+
+struct OverlookView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @StateObject var viewModel: OverlookViewModel = .init()
+    
+    var body: some View {
+        
+        ZStack {
+            Color("Background")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 0) {
+                
+                AppBar {
+                    
+                } content : {
+                    ZStack {
+                        
+                        Text("Detail")
+                            .offset(y: abs(viewModel.offset) < viewModel.avatarHeight ? 0 : -25)
+                            .opacity(abs(viewModel.offset) < viewModel.avatarHeight ? 1 : 0)
+                        
+                        Text("Design Talk: UI/UX Sharing Session")
+                            .offset(y: abs(viewModel.offset) >= viewModel.avatarHeight ? 0 : 25)
+                            .opacity(abs(viewModel.offset) >= viewModel.avatarHeight ? 1 : 0)
+                        
+                    }
+                    .animation(.default, value: viewModel.offset)
+                    .lineLimit(1)
+                    .font(.headline)
+                    .foregroundColor(Color("Text"))
+                    .clipped()
+                }
+                            
+                ScrollView(showsIndicators: false) {
+                    ZStack {
+                        
+                        ScrollOffserListener { offset in
+                            viewModel.offset = offset
+                        }
+                        
+                        
+                        VStack(spacing: 0) {
+                            
+                            OverlookAvatar()
+                                .frame(width: viewModel.width * 0.75)
+                                .frame(maxWidth: .infinity)
+                                .sizeDetector(height: $viewModel.avatarHeight)
+                            
+                            SizeBox(height: 40)
+                            
+                            OverDescription()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            Color.clear
+                                .frame(height: 1000)
+                            
+                        }
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .coordinateSpace(name: "scroll")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                OverlookFriends()
+                
+            }
+        }
+        .sizeDetector(width: $viewModel.width)
+    }
+}
+
+struct OverlookView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            OverlookView()
+                .environment(\.colorScheme, .light)
+            
+            OverlookView()
+                .environment(\.colorScheme, .dark)
+        }
+    }
+}
