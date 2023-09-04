1a94730f7715508c9feee2522d2e3ea40e421ea3
Guen <heloo@guen.dev>
2023-09-04 19:37:34 +0700

    cm

--- lingoes/Screens/Overlook/Views/OverlookLevelItem.swift
+++ lingoes/Screens/Overlook/Views/OverlookLevelItem.swift
@@ -14,56 +14,60 @@ struct OverlookLevelItem: View {
     @State var isChecked: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
+        VStack {
             
-            Text(level.title)
-                .font(.headline)
-                .fontWeight(.regular)
-                .kerning(0.5)
-                .foregroundColor(Color("Text"))
-            
-            Text(level.subtitle)
-                .font(.subheadline)
-                .foregroundColor(Color("Text2"))
-            
-        }
-        .padding(.horizontal, 20)
-        .padding(.vertical)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .background(colorScheme == .light ? .white : Color("Background2"))
-        .cornerRadius(15)
-        .shadow(color: Color(red: 0.02, green: 0.01, blue: 0.29).opacity(0.03), radius: 27, x: 10, y: 24)
-        .overlay(alignment: .trailing) {
-            
-            Button {
+            VStack(alignment: .leading, spacing: 8) {
                 
-                withAnimation {
-                    isChecked.toggle()
-                }
+                Text(level.title)
+                    .font(.headline)
+                    .fontWeight(.regular)
+                    .kerning(0.5)
+                    .foregroundColor(Color("Text"))
                 
-            } label: {
+                Text(level.subtitle)
+                    .font(.subheadline)
+                    .foregroundColor(Color("Text2"))
                 
-                Circle()
-                    .fill(isChecked ? Color.accentColor : .clear)
-                    .frame(width: 20, height: 20)
-                    .cornerRadius(50)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 50)
-                            .inset(by: 0.5)
-                            .stroke(isChecked ? Color.accentColor : Color("Text2").opacity(0.5), lineWidth: 1)
-                    }
-                    .overlay {
-                        Image(systemName: "checkmark")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(isChecked ? Color.white : .clear)
-                            .scaleEffect(isChecked ? 1 : 0)
-                    }
-                    .offset(x: -20)
             }
-            .buttonStyle(.plain)
+            .padding(.horizontal, 20)
+            .padding(.vertical)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(colorScheme == .light ? .white : Color("Background2"))
+            .cornerRadius(15)
+            .shadow(color: Color(red: 0.02, green: 0.01, blue: 0.29).opacity(0.03), radius: 27, x: 10, y: 24)
+            .overlay(alignment: .trailing) {
+                
+                Button {
+                    
+                    withAnimation {
+                        isChecked.toggle()
+                    }
+                    
+                } label: {
+                    
+                    Circle()
+                        .fill(isChecked ? Color.accentColor : .clear)
+                        .frame(width: 20, height: 20)
+                        .cornerRadius(50)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 50)
+                                .inset(by: 0.5)
+                                .stroke(isChecked ? Color.accentColor : Color("Text2").opacity(0.5), lineWidth: 1)
+                        }
+                        .overlay {
+                            Image(systemName: "checkmark")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(isChecked ? Color.white : .clear)
+                                .scaleEffect(isChecked ? 1 : 0)
+                        }
+                        .offset(x: -20)
+                }
+                .buttonStyle(.plain)
 
+                
+            }
             
         }
     }
