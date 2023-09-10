58d9be5893bded49563747446bdaaae865b71a8f
Guen <heloo@guen.dev>
2023-09-10 21:46:59 +0700

    add basic game

--- lingoes/Screens/Game/Views/GameImageText.swift
+++ lingoes/Screens/Game/Views/GameImageText.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct GameImageText: View {
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(maximum: 120), spacing: 32),
+        GridItem(.flexible(maximum: 120), spacing: 32) // Set a different spacing value for the Y axis
+    ]
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             
+            SizeBox(height: 30)
+            
+            VStack(spacing: 5) {
+                
+                Text("What is")
+                    .font(.body)
+                    .foregroundColor(Color("Text2"))
+                
+                Text("\"Apple\"")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("Text"))
+            }
+            
+            SizeBox(height: 30)
+            
+            LazyVGrid(columns: columns, spacing: 32) {
+                ForEach(0..<4) { index in
+                    
+                    Sizeable { size in
+                        
+                        Button {
+                            
+                        } label: {
+                            
+                            Rectangle()
+                                .fill(.clear)
+                                .frame(width: size.width, height: size.width)
+                                .overlay {
+                                    
+                                    Image("avatar")
+                                        .resizable()
+                                        .scaledToFit()
+                                    
+                                }
+                                .clipShape(RoundedRectangle(cornerRadius: 15))
+                            
+                        }
+                        .buttonStyle(.plain)
+
+                        
+                    }
+                    
+                }
+            }
         }
     }
 }
